2e40f0fd0cfbee4070141d29f027fb201f76c531
tytanus97 <tytanus97@gmail.com>
2020-07-04 17:22:36 +0200

    add plane form

--- src/app/components/add-plane/add-plane.component.ts
+++ src/app/components/add-plane/add-plane.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-plane',
@@ -6,10 +7,39 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./add-plane.component.css']
 })
 export class AddPlaneComponent implements OnInit {
+  public planeForm;
+  constructor(private fb: FormBuilder) {
 
-  constructor() { }
+    this.planeForm = this.fb.group({
+      planeName: ['', { validators: [Validators.required, Validators.minLength(1), Validators.maxLength(50)], updateOn: 'blur' }],
+      planeColor: ['', { validators: [Validators.required, Validators.minLength(3), Validators.maxLength(25)], updateOn: 'blur' }],
+      planeWingSpan: ['', { validators: [Validators.required, Validators.min(0.1), Validators.max(5)], updateOn: 'blur' }],
+      planeWeight: ['', { validators: [Validators.required, Validators.min(0), Validators.max(5)], updateOn: 'blur' }]
+    });
+
+  }
 
   ngOnInit(): void {
+  }
+
+  planeFormSubmit() {
+
+  }
+
+  get planeName() {
+    return this.planeForm.get('planeName');
+  }
+
+  get planeColor() {
+    return this.planeForm.get('planeColor');
+  }
+
+  get planeWingSpan() {
+    return this.planeForm.get('planeWingSpan');
+  }
+
+  get planeWeight() {
+    return this.planeForm.get('planeWeight');
   }
 
 }
