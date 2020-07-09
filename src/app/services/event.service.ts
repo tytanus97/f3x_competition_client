4c107d690dfb658834599d21fb13be47a060b724
tytanus97 <tytanus97@gmail.com>
2020-07-09 22:38:10 +0200

    initial event logic implementation

---
+++ src/app/services/event.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  private _url = 'http://localhost:8080/api/events/';
+
+  constructor(private http: HttpClient) { }
+
+
+  public addEvent(event: Event) {
+    return this.http.post(this._url, event, {observe: 'response'});
+  }
+
+  public getEvent(eventId: number) {
+    return this.http.get<Event>(this._url + `${eventId}`, {observe: 'response'});
+  }
+
+  public getAllEvents() {
+    return this.http.get<Array<Event>>(this._url, {observe: 'response'});
+   }
+
+   public updateEvent(event: Event) {
+     return this.http.put(this._url + `/`, event, {observe: 'response'});
+   }
+
+}
