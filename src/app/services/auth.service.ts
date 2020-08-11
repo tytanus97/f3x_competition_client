512ee0a8c2e194d23367ec8a5f8e0ab1e85bb0cb
tytanus97 <tytanus97@gmail.com>
2020-08-11 17:32:54 +0200

    implement bad credentials error

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { PilotService } from './pilot.service';
 import { PilotCredentials } from 'src/app/models/PilotCredentials';
 
@@ -6,6 +6,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Pilot } from '../models/Pilot';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -15,6 +16,7 @@ export class AuthService {
 
   private _url = 'http://localhost:8080/api/auth/';
   public loggedPilot = new BehaviorSubject<Pilot>(null);
+
   constructor(private httpClient: HttpClient, private router: Router, private pilotService: PilotService) { }
 
   isLogged(): boolean {
@@ -22,10 +24,7 @@ export class AuthService {
   }
 
   authenticate(credentials: PilotCredentials) {
-
-      console.log(credentials);
-      this.httpClient.post(this._url + 'authenticate', credentials).subscribe(res => this.setToken(res), err => {});
-
+    return this.httpClient.post(this._url + 'authenticate', credentials);
   }
 
   getToken(): string {
@@ -33,14 +32,13 @@ export class AuthService {
   }
 
   setToken(response) {
-
     const token = response.jwt;
     const loggedPilotId = response.pilotId;
     localStorage.setItem('token', token);
     localStorage.setItem('loggedPilotId', loggedPilotId);
 
     this.pilotService.getPilotById(loggedPilotId).subscribe(response => this.loggedPilot.next(response.body));
-    
+
     this.router.navigate(['/home']);
   }
 
