93ea32e606b65ee13bd0b8ee33bfbc28e642c0af
tytanus97 <tytanus97@gmail.com>
2020-08-21 18:09:39 +0200

    implement event add

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Event } from 'src/app/models/Event';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class EventService {
 
   constructor(private http: HttpClient) { }
 
-  
+
   public addEvent(event: Event) {
     return this.http.post(this._url, event, {observe: 'response'});
   }
