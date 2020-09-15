417b138038bb04cec3a58570ff5536ea1fab3268
tytanus97 <tytanus97@gmail.com>
2020-09-15 16:30:29 +0200

    refactor nav add responsive, change delete flight icon

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,11 +8,39 @@ import { Router } from '@angular/router';
 })
 export class NavigationComponent implements OnInit {
 
+  public display: string = 'flex';
+  public prevWidth: number = window.innerWidth;
+
   constructor(private router: Router) { }
   
   ngOnInit(): void {
   }
 
 
+  public toggle() {
+    console.log('toggl;e');
+    this.display = this.display === 'flex'?'none':'flex';
+    console.log(this.display);
+  }
+  hide() {
+    if(window.innerWidth <= 990) {
+    this.display = 'none';
+    }
+  }
+
+  show() {
+    this.display = 'flex';
+   }
+
+   @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if(window.innerWidth <= 990 && this.prevWidth > 990) {
+      this.display = 'none';
+    } else if (window.innerWidth > 990 && this.prevWidth <=990) {
+      this.display = 'flex';
+    }
+  this.prevWidth = window.innerWidth;
+}
+ 
 
 }
